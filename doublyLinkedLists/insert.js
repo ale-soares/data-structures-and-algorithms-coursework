22c61eba91d5aa6f7438c0f314f947fb511d0d37
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-01-22 18:22:39 -0300

    add set and insert methods

---
+++ doublyLinkedLists/insert.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    const newNode = new Node(value);
+    const before = this.length(index - 1);
+    const after = before.next;
+
+    before.next = newNode;
+    newNode.prev = before;
+    after.prev = newNode;
+    newNode.next = after;
+
+    this.length++;
+    return true;
+  }
+}
