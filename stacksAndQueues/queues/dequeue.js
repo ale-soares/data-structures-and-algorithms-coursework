b0635d2bc6d4337d52fb1a0765f166cf732004c5
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-01-24 16:28:38 -0300

    add dequeue method

---
+++ stacksAndQueues/queues/dequeue.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+
+  dequeue() {
+		if (this.length === 0) return undefined
+
+		let temp = this.first
+
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next
+			temp.next = null
+    }
+
+    this.length--;
+    return temp;
+  }
+}
