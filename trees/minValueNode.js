46979057e3b51715f4653d26b7df340a35b14c13
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-01-29 12:55:47 -0300

    add min value node

---
+++ trees/minValueNode.js
@@ -0,0 +1,21 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  minValueNode(currentNode) {
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    
+    return currentNode;
+  }
+}
