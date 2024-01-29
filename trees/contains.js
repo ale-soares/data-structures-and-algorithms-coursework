c7749b37163778b5115f7895b2fc9e4bc04fb985
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-01-29 12:43:13 -0300

    add contains method

---
+++ trees/contains.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  contains(value) {
+    if (this.root === null) return false;
+
+    let temp = this.root;
+
+    while (temp) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
