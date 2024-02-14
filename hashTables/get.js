503457595ff5ad8ac9eb56186f07c9d297f4899c
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-02-14 12:34:40 -0300

    add get method

---
+++ hashTables/get.js
@@ -0,0 +1,29 @@
+class HashTable {
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+    }
+
+    return hash;
+  }
+
+  get(key) {
+    let index = this._hash(key);
+
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
+}
