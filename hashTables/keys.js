a4ea9329daadb1f4b9c0a446a40afea17870090c
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-02-14 15:46:19 -0300

    add keys method

---
+++ hashTables/keys.js
@@ -0,0 +1,29 @@
+class HashTable {
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+    }
+
+    return hash;
+  }
+
+  keys() {
+    let allKeys = [];
+
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          allKeys.push(this.dataMap[i][j][0]);
+        }
+      }
+    }
+
+    return allKeys;
+  }
+}
