ab96da82342627296fd03b85b92f76f7a6016176
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-02-20 11:27:29 -0300

    add graphs edge and vertex methods

---
+++ graphs/addEdge.js
@@ -0,0 +1,15 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
+
+    return false;
+  }
+}
