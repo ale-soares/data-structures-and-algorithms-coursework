f10acc8886d9e47c75459f56bb943e8fb9dfbd3d
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-04-08 16:09:20 -0300

    format quick sort

--- sortingAlgorithms/quickSort/pivot.js
+++ sortingAlgorithms/quickSort/pivot.js
@@ -1,20 +1,20 @@
 function swap(array, firstIndex, secondIndex) {
-  let temp = array[firstIndex]
+  let temp = array[firstIndex];
 
-  array[firstIndex] = array[secondIndex]
-  array[secondIndex] = temp
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
 }
 
-function pivot(array, pivotIndex=0, endIndex=array.length-1) {
-  let swapIndex = pivotIndex
+function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
+  let swapIndex = pivotIndex;
 
   for (let i = pivotIndex + 1; i <= endIndex; i++) {
     if (array[i] < array[pivotIndex]) {
-      swapIndex++
-      swap(array, swapIndex, i)
+      swapIndex++;
+      swap(array, swapIndex, i);
     }
   }
 
-  swap(array, pivotIndex, swapIndex)
-  return swapIndex
+  swap(array, pivotIndex, swapIndex);
+  return swapIndex;
 }
